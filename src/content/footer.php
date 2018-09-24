b8dc2fafa4e23441df9298d1dff01d10a7a5b95e
Edugolr <christofer.wikman@gmail.com>
2018-09-24 10:43:20 +0100

    1.0.2 flyttade scripts till footer filen i content. Påbörjat implementation utav ContentTools

---
+++ src/content/footer.php
@@ -0,0 +1,7 @@
+<script src="js/jquery.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="assets/content-tools.min.js"></script>
+<script src="assets/editor.js"></script>
+</body>
+</html>
--- src/content/header.php
+++ src/content/header.php
@@ -6,7 +6,9 @@
 	    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:400,800">
         <link rel='stylesheet' href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="assets/content-tools.min.css">
         <link rel="stylesheet" href="css/bootstrap.css">
         <link rel="stylesheet" href="css/styles.css">
+
     </head>
 <body>
